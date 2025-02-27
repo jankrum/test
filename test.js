3cced491c5b28a05a6b7e8728803937c56f31aa0
jankrum <professionalninety@gmail.com>
2025-02-27 14:43:48 -0800

    Updated dm func

--- test.js
+++ test.js
@@ -1,4 +1,12 @@
-function dm(tag, attributes = {}, ...children) {
+/**
+ * Creates a DOM element with the specified tag, attributes, and children.
+ *
+ * @param {string} tag - The type of element to create (e.g., 'div', 'span').
+ * @param {Object.<string, string>} [attributes={}] - An object containing key-value pairs of attributes to set on the element.
+ * @param {...(string|HTMLElement)} children - The children to append to the created element. Can be strings or DOM elements.
+ * @returns {HTMLElement} The created DOM element.
+ */
+export default function dm(tag, attributes = {}, ...children) {
     const element = document.createElement(tag)
 
     for (const [key, value] of Object.entries(attributes)) {
